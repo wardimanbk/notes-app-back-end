2adbb2cf9f0ea83b44ae329bda686c31ad1a7b6a
wardimanwbk <wardimanwbk@gmail.com>
2023-04-16 14:27:20 +0800

    restructure with hapi plugin

---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,74 @@
+const { nanoid } = require('nanoid');
+
+class NotesService{
+  constructor(){
+    this._notes = [];
+  }
+
+  addNote({title, body, tags}){
+
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+      title, tags, body, id, createdAt, updatedAt,
+    };
+
+    this._notes.push(newNote);
+
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error('Catatan gagal ditambahkan');
+    }
+ 
+    return id;
+
+
+  }
+
+  getNotes(){
+    return this._notes;
+  }
+
+  getNoteById(id){
+    const note = this._notes.filter((n) => n.id === id)[0];
+
+    if (!note) {
+      throw new Error('Catatan tidak ditemukan');
+    }
+    return note;
+  }
+
+  editNoteById(id, { title, body, tags }){
+    const updatedAt = new Date().toISOString();
+
+    const index = this._notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+      this._notes[index] = {
+        ...this._notes[index],
+        title,
+        tags,
+        body,
+        updatedAt,
+      };
+    } else {
+      throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+  }
+
+  deleteNoteById(id){
+    const index = this._notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+      this._notes.splice(index, 1);
+    } else {
+      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+    }
+  }
+
+}
+
+module.exports = NotesService;
